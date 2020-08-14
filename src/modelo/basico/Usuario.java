2a5fa466f645ec7a21c857a4f35eee7db6dddee4
Valdir Barbosa da Silva Junior <valdir.barbosasistemas@gmail.com>
2020-08-14 12:55:02 -0300

    Criação do Projeto em JPA
    criação da classe para o Projeto JPA

---
+++ src/modelo/basico/Usuario.java
@@ -0,0 +1,58 @@
+package modelo.basico;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*
+ * Significa que essa classe ela é uma entidade de um banco de dados, ou seja, a
+ * tabela que irá ser criada terá o mesmo nome da classe. Assim que terminar as annotations, 
+ * DEVE-SE ir no arquivo persistence.xml para referenciar a classe Usuario no banco de dados
+ */
+@Entity
+public class Usuario {
+	@Id // Significa que o atributo irá ser a chave primária do banco de dados
+	private Long id;
+
+	private String nome;
+
+	private String email;
+
+	// Construtor padrão
+	public Usuario() {
+
+	}
+
+	// Construtor
+
+	public Usuario(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+	}
+
+	// Getters e Setters
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
