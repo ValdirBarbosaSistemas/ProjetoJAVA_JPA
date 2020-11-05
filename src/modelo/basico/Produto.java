3dae9dc9779a9e515a9b652d23d4127e374612e5
Valdir <valdir.barbosasistemas@gmail.com>
2020-11-05 20:18:12 -0300

    Mudanças no persistence.xml e criacao de uma nova entidade PRODUTO

---
+++ src/modelo/basico/Produto.java
@@ -0,0 +1,57 @@
+package modelo.basico;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produtos") // Modificado o nome da tabela
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "prod_nome") // Modificado o nome da coluna
+	private String nome;
+
+	@Column(name = "prod_preco") // Modificando o nome preco
+	private Double preco;
+
+	public Produto() {
+
+	}
+
+	public Produto(String nome, Double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return this.preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+}
