12f7207670ba1cc8f49220eeed3d88bc3712eab6
Valdir <valdir.barbosasistemas@gmail.com>
2020-12-15 12:26:23 -0300

    Criação do relacionamento um para um em JPA

---
+++ src/modelo/relacionamentoUmPraUm/Cliente.java
@@ -0,0 +1,70 @@
+package modelo.relacionamentoUmPraUm;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+
+	/*
+	 * Neste caso estamos querendo fazer um tipo de relacionamento "UNIDIRECINAL",
+	 * ou seja, iremos focar em apenas um lado. Veremos no código abaixo...
+	 * 
+	 */
+
+	@OneToOne
+	@JoinColumn(name = "assento_id", unique = true) // Desse modo a chave será ÚNICA não havendo duplicidade
+	private Assento assentos; // Dessa maneira estarei tendo um acesso a classe Assento como um todo
+	/*
+	 * Dessa maneira no código acima, ele irá criar uma 'chave estrangeira' de nome
+	 * 'assento_id' na classe Cliente, pois além de trazer os dados da classe
+	 * Assento, ele também faz de forma automática devido a annotation "OneToOne".
+	 */
+
+	// Construtor padrão
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(String nome, Assento assentos) {
+		super();
+		this.nome = nome;
+		this.assentos = assentos;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Assento getAssentos() {
+		return this.assentos;
+	}
+
+	public void setAssentos(Assento assentos) {
+		this.assentos = assentos;
+	}
+
+}
---
+++ src/teste/relacionamentoUmPraUm/NovoClienteAssento1.java
@@ -0,0 +1,30 @@
+package teste.relacionamentoUmPraUm;
+
+import infra.DAO;
+import modelo.relacionamentoUmPraUm.Assento;
+import modelo.relacionamentoUmPraUm.Cliente;
+
+public class NovoClienteAssento1 {
+	public static void main(String[] args) {
+		// Criando o assento...
+		Assento assento = new Assento("19C");
+		// Criando o cliente...
+		Cliente cliente = new Cliente("Carlos", assento);
+
+		DAO<Object> dao = new DAO<Object>();
+
+		// Criando a transação para se colocar no banco
+		dao.abrirTransacao()
+		.incluirTransacao(assento)
+		.incluirTransacao(cliente)
+		.fecharTransacao()
+		.fechar();
+		/*
+		 * Lembrando que estamos querendo um tipo de relacionamento UM PRA UM, ou seja,
+		 * não poderá haver duplicidade de código ref. ao assento. E Devido a isso por
+		 * padrão, o relacionamento "OneToOne" ele não cria a chave UNIQUE, ou seja,
+		 * vamos ter que criar na classe Cliente uma annotation chamada "JoinColumn" que
+		 * ele é específico para mapear campos que representam junção de tabelas.
+		 */
+	}
+}
